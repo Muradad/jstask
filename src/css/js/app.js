5f597b42ca4e4155e7a8ea22ffa3e9ae320fa6fa
Murad99 <mrmurat099@gmail.com>
2023-09-19 16:40:08 +0400

    taskend

---
+++ src/css/js/app.js
@@ -0,0 +1,40 @@
+// Task 1 
+// let rengDeyissin = confirm('rengi deyis');
+
+// if(rengDeyissin){
+//     document.body.style.backgroundColor = 'black';
+// }
+// else{
+//     document.body.style.backgroundColor = 'red';
+// }
+
+// Task 2 
+// let yasiniDenen = prompt('yasini qeyd et');
+
+// if(yasiniDenen <18){
+//     alert('prava ala bilmersen');
+// }
+// else{
+//     alert('ala bilersen');
+// }
+
+
+
+// Task 3
+// class users{
+//     constructor(ad,soyad,password){
+//         this.ad = ad;
+//         this.soyad = soyad;
+//         this.password = password;
+//     }
+
+//     getFullName(){
+//         let fullName = this.ad + '.' + this.soyad+"." + this.password;
+//         this.pas
+//         let uppercaseName = fullName.toUpperCase();
+//         document.write(`<h1>${uppercaseName}</h1>`)
+//     }
+// }
+
+// let user = new users('murad','memmedov','Sifre:1999');
+// user.getFullName();
